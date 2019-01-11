48bb6ced8bc5b3f6adad18686175f64a84bd39d2
Roger <luojie@w-ew.com>
2019-01-11 14:42:40 +0800

    添加iview ui

--- testVue/src/main.js
+++ testVue/src/main.js
@@ -1,13 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import App from 'components/app.vue'
+import Routers from './router.js'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(iView)
+
+// The routing configuration
+const RouterConfig = {
+  routes: Routers
+}
+const router = new VueRouter(RouterConfig)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  router: router,
+  render: h => h(App)
 })
