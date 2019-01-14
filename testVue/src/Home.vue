3826a9ac1cee75b75465f47fcd1cefe947224399
viiyy1234 <543845440@qq.com>
2019-01-14 17:14:53 +0800

    删除test和test2修改home

--- testVue/src/Home.vue
+++ testVue/src/Home.vue
@@ -9,7 +9,6 @@
 .layout-logo{
     width: 100px;
     height: 30px;
-    background: #5b6270;
     border-radius: 3px;
     float: left;
     position: relative;
@@ -17,9 +16,15 @@
     left: 20px;
 }
 .layout-nav{
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
+    width: 600px;
+    margin-left: 150px;
+}
+#title{
+    font-size: 20px;
+    font-family: "微软雅黑";
+    font-weight: bold;
+    color: #f5f7f9;
+    top: 0px;
 }
 </style>
 <template>
@@ -27,23 +32,31 @@
         <Layout>
             <Header>
                 <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo"></div>
+                    <div id="title" class="layout-logo">智能巡检系统</div>
                     <div class="layout-nav">
                         <MenuItem name="1">
                             <Icon type="ios-navigate"></Icon>
-                            Item 1
+                            1
                         </MenuItem>
                         <MenuItem name="2">
                             <Icon type="ios-keypad"></Icon>
-                            Item 2
+                            2
                         </MenuItem>
                         <MenuItem name="3">
                             <Icon type="ios-analytics"></Icon>
-                            Item 3
+                            3
                         </MenuItem>
                         <MenuItem name="4">
                             <Icon type="ios-paper"></Icon>
-                            Item 4
+                            4
+                        </MenuItem>
+                        <MenuItem name="5">
+                            <Icon type="ios-paper"></Icon>
+                            5
+                        </MenuItem>
+                        <MenuItem name="6">
+                            <Icon type="ios-paper"></Icon>
+                            6
                         </MenuItem>
                     </div>
                 </Menu>
@@ -92,3 +105,4 @@
         </Layout>
     </div>
 </template>
+
