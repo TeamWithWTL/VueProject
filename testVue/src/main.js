89912369b04469a67b4bfc8e7acf06bdbfea68c4
Roger <luojie@w-ew.com>
2019-01-14 11:47:47 +0800

    1

--- testVue/src/main.js
+++ testVue/src/main.js
@@ -1,11 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Axios from 'axios'
 import App from './App'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 
 Vue.use(iView)
+Vue.use(VueRouter)
+Vue.prototype.$axios = Axios //引入Axios不能使用Vue.use
 
 /* eslint-disable no-new */
 new Vue({
